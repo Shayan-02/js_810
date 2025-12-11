b93600ec4c94ac67f9cf2143f8897dc3f1420780
Shayan Shahbazi <upm200@gmail.com>
2025-12-11 11:48:15 +0330

    .

---
+++ test/assets/js/script.js
@@ -0,0 +1,30 @@
+// Add your JavaScript code here
+const express = require("express");
+const mysql = require("mysql");
+// import mysql from mysql2
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "node_shop1",
+});
+
+db.connect(function (err) {
+  if (err) console.error(err);
+  console.log("Connected!");
+  db.query(
+    "CREATE TABLE users (name VARCHAR(255), address VARCHAR(255))",
+    function (err, result) {
+      if (err) console.error(err);
+      console.log("table created");
+    }
+  );
+});
+
+app.listen(3000, (err) => {
+  if (err) console.error(err);
+  console.log("port 3000 connected!");
+});
